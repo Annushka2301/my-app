089e89242e51f2551e46ec20ad54b9149420db3b
Anna <morozovaannuska@mail.ru>
2022-06-07 17:58:04 +0300

    change pages

---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Error() {
+    return (
+        <h1 style={{color:'red'}}>
+            the page does not exist
+        </h1>
+    )
+}
+
+export default Error;
